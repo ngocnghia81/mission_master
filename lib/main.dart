9dd74e93520a40ce7f62b9419adb70631379ad8e
arignrika <arignrika@gmail.com>
2025-05-02 01:32:58 +0700

    .

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:mission_master/features/auth/screens/login_screen.dart';
 import 'package:mission_master/features/auth/screens/register_screen.dart';
 import 'package:mission_master/features/home/screens/calendar_task_screen.dart';
+import 'package:mission_master/features/home/screens/employee_home_screen.dart';
 import 'package:mission_master/features/manager/screens/create_project_screen.dart';
 import 'package:mission_master/features/tasks/screens/task_list_screen.dart';
 import 'package:mission_master/features/projects/screens/project_list_screen.dart';
@@ -33,15 +34,16 @@ class MyApp extends StatelessWidget {
         ),
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: '/login',
-      routes: {
-        '/': (context) => LoginScreen(),
-        '/login': (context) => LoginScreen(),
-        '/register': (context) => RegisterScreen(),
-        '/home': (context) => CalendarTaskScreen(),
-        '/tasks': (context) => const TaskListScreen(),
-        '/projects': (context) => const ProjectListScreen(),
-      },
+      // initialRoute: '/login',
+      // routes: {
+      //   '/': (context) => LoginScreen(),
+      //   '/login': (context) => LoginScreen(),
+      //   '/register': (context) => RegisterScreen(),
+      //   '/home': (context) => CalendarTaskScreen(),
+      //   '/tasks': (context) => const TaskListScreen(),
+      //   '/projects': (context) => const ProjectListScreen(),
+      // },
+      home: EmployeeHomeScreen(),
     );
   }
 }
